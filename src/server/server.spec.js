bc6da523ea7b61c4a8b1ae0df3a19feb353d433a
Irena Jurasek <irenaskydive@gmail.com>
2020-04-30 19:01:22 +0200

    Server update task

--- src/server/server.spec.js
+++ src/server/server.spec.js
@@ -1,6 +1,12 @@
-import { addNewTask } from "./server";
+import { addNewTask, updateTask } from "./server";
 
-addNewTask({
-  name: "My task",
-  id: "12345",
-});
+(async function myFunc() {
+  await addNewTask({
+    name: "My task",
+    id: "12346",
+  });
+  await updateTask({
+    name: "Updated task",
+    id: "12346",
+  });
+})();
