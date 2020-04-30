14d60003e542b852d2668cf9715b66efa5f4d438
Irena Jurasek <irenaskydive@gmail.com>
2020-04-30 22:03:52 +0200

    Add authentication to server

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { connectDB } from "./connect-db";
 import "./initialize-db";
+import { authenticationRoute } from "./authenticate";
 
 let port = 7777;
 let app = express();
@@ -14,6 +15,8 @@ app.listen(port, console.log("Server listening on", port));
 // });
 
 app.use(cors(), bodyParser.urlencoded({ extended: true }), bodyParser.json());
+
+authenticationRoute(app);
 
 export const addNewTask = async (task) => {
   let db = await connectDB();
