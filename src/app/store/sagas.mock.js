8e6e788a4068804e2b6b5a9236f9e9826b18ff00
Irena Jurasek <irenaskydive@gmail.com>
2020-04-30 15:29:20 +0200

    Create task detail component

--- src/app/store/sagas.mock.js
+++ src/app/store/sagas.mock.js
@@ -5,8 +5,9 @@ import { v4 as uuid } from "uuid";
 export function* taskCreationSaga() {
   while (true) {
     const { groupID } = yield take(mutations.REQUEST_TASK_CREATION);
-    const ownerID = yield select((state) => state.session.id);
+    const ownerID = `U1`;
     const taskID = uuid();
     yield put(mutations.createTask(taskID, groupID, ownerID));
+    console.log("Got groupID", groupID);
   }
 }
