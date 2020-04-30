40b40b6a190be8ba36f603d20c09f1d1b58dad0d
Irena Jurasek <irenaskydive@gmail.com>
2020-04-30 23:15:26 +0200

    Update for production

--- src/server/connect-db.js
+++ src/server/connect-db.js
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-const url = `mongodb://localhost:27017/organizer`;
+const url = process.env.MONGODB_URI || `mongodb://localhost:27017/organizer`;
 let db = null;
 
 export async function connectDB() {
